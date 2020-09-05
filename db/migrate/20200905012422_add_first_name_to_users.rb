735e40301f6f4ffbe61015d3f8458e1e37c46a6c
yutaro-nakaji <quadcore.4412gmail.com>
2020-09-05 10:28:51 +0900

    usersテーブル作成

---
+++ db/migrate/20200905012422_add_first_name_to_users.rb
@@ -0,0 +1,9 @@
+class AddFirstNameToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :first_name_kana, :string
+    add_column :users, :last_name_kana, :string
+    add_column :users, :birthday, :date
+  end
+end
