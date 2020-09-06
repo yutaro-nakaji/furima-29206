e9cd0b12843c1f01d6d84fc1e6122243ca9b7092
yutaro-nakaji <quadcore.4412gmail.com>
2020-09-06 18:01:17 +0900

    itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :explanation
+    validates :category_id
+    validates :status_id
+    validates :shipping_charges_id
+    validates :shipping_region_id
+    validates :days_until_shipping_id
+  end
+  validates :price, presence: true, inclusion: {in: 300..9999999 }
 end
