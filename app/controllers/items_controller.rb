b7d578da66041824112c8b133ed97fc9341d00f2
yutaro-nakaji <quadcore.4412gmail.com>
2020-09-06 17:50:42 +0900

    itemsコントローラー作成（2）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,12 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :status_id, :shipping_charges_id, :shipping_region_id, :days_until_shipping_id, :price, :user_id)
+  end
 end
