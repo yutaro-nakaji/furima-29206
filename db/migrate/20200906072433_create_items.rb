09314c24c10502d9d4b817b2c879427ee0858552
yutaro-nakaji <quadcore.4412gmail.com>
2020-09-06 16:41:09 +0900

    itemsテーブル作成

--- db/migrate/20200906072433_create_items.rb
+++ db/migrate/20200906072433_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :name
+      t.text :explanation
+      t.integer :category_id
+      t.integer :status_id
+      t.integer :shipping_charges_id
+      t.integer :shipping_region_id
+      t.integer :days_until_shipping_id
+      t.integer :price
+      t.integer :user_id, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_05_012422) do
+ActiveRecord::Schema.define(version: 2020_09_06_072433) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.text "explanation"
+    t.integer "category_id"
+    t.integer "status_id"
+    t.integer "shipping_charges_id"
+    t.integer "shipping_region_id"
+    t.integer "days_until_shipping_id"
+    t.integer "price"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
