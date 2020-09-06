0ab43a1aeac9b1783ae8ccec16e3e41faf8198a4
yutaro-nakaji <quadcore.4412gmail.com>
2020-09-06 16:43:37 +0900

    itemsモデルのルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :items
 end
