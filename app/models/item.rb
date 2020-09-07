cf96916643bf0b3670c91448038321d889c9d77d
yutaro-nakaji <quadcore.4412gmail.com>
2020-09-07 10:27:27 +0900

    Statusモデル導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,12 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
+  belongs_to_active_hash :status
 
   validates :name, presence: true
   validates :explanation, presence: true
   validates :category_id, presence: true, numericality: { other_than: 1 } 
-  validates :status_id
+  validates :status_id, presence: true, numericality: { other_than: 1 } 
   validates :shipping_charges_id
   validates :shipping_region_id
   validates :days_until_shipping_id
