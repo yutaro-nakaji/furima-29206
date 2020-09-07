e9979b4ad3d6084f35eb5f0953421cff0314fb33
yutaro-nakaji <quadcore.4412gmail.com>
2020-09-07 18:55:13 +0900

    価格計算_非同期通信の実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,23 @@
+function price_calc(){
+  //HTMLの要素取得（販売価格・手数料・利益）
+  const price = document.getElementById("item-price");
+  const tax = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+  //販売価格に入力があった時イベント発火
+  price.addEventListener('input', () => {
+    //valueプロパティで販売価格の取得
+    const value = price.value; 
+    if (value >= 300 && value <= 9999999){
+      let fee = Math.floor(value * 0.1)
+      let gains = value - fee
+      tax.textContent = fee;
+      profit.textContent = gains;
+    } else {
+      let fee = '-';
+      let gains = '-';
+      tax.textContent = fee;
+      profit.textContent = gains;
+    }
+  });
+}
+window.addEventListener('load', price_calc);
