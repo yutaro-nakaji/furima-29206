88ffb14b4e015dff2a7cb7a13b60066ec2117799
yutaro-nakaji <quadcore.4412gmail.com>
2020-09-07 10:59:42 +0900

    DaysUntilShippingモデル導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :status
   belongs_to_active_hash :shippingcharge
   belongs_to_active_hash :shippingregion
+  belongs_to_active_hash :daysuntilshipping
 
   validates :name, presence: true
   validates :explanation, presence: true
@@ -13,7 +14,7 @@ class Item < ApplicationRecord
   validates :status_id, presence: true, numericality: { other_than: 1 }
   validates :shipping_charges_id, presence: true, numericality: { other_than: 1 }
   validates :shipping_region_id, presence: true, numericality: { other_than: 1 }
-  validates :days_until_shipping_id
+  validates :days_until_shipping_id, presence: true, numericality: { other_than: 1 }
   PRICE_REGEX = /\A[0-9]+\z/.freeze
   validates :price, presence: true, inclusion: {in: 300..9999999 }, format: { with: PRICE_REGEX }
 end
