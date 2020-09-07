2fd34cc5f56d9c5f6016af9388710059efa355c7
yutaro-nakaji <quadcore.4412gmail.com>
2020-09-07 10:16:39 +0900

    Categoryモデル導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
 
-  with_options presence: true do
-    validates :name
-    validates :explanation
-    validates :category_id
-    validates :status_id
-    validates :shipping_charges_id
-    validates :shipping_region_id
-    validates :days_until_shipping_id
-  end
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 } 
+  validates :status_id
+  validates :shipping_charges_id
+  validates :shipping_region_id
+  validates :days_until_shipping_id
   PRICE_REGEX = /\A[0-9]+\z/.freeze
   validates :price, presence: true, inclusion: {in: 300..9999999 }, format: { with: PRICE_REGEX }
 end
